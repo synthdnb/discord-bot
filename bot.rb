149991f649308b1209c3baef328d4fd45faa4621
Minkyu Kim <minkyu.kim@devsisters.com>
2018-01-04 14:59:15 +0900

    Search keyword when not found

--- bot.rb
+++ bot.rb
@@ -19,6 +19,8 @@ def client
     @client ||= Discordrb::Bot.new token: ENV['TOKEN'], client_id: ENV['CLIENT_ID']
 end
 
+Discordrb::LOGGER.mode = :debug
+
 reserved = %w(공지 등록 삭제 목록)
 
 client.message(with_text: /\A!(\S+)(.*)/m) do |event|
@@ -30,7 +32,7 @@ client.message(with_text: /\A!(\S+)(.*)/m) do |event|
     when "목록"
         event << keywords.sort.join(", ")
     when "등록"
-        next unless event.channel.type == 1
+        # next unless event.channel.type == 1
         data = {}
 
         if event.message.attachments.empty?
@@ -55,16 +57,31 @@ client.message(with_text: /\A!(\S+)(.*)/m) do |event|
             event << "내용을 입력해주세요"
         end
     when "삭제"
-        next unless event.channel.type == 1
+        # next unless event.channel.type == 1
         next unless content.match /\A(\S+)(.*)/m
         key = $1
-        redis.hdel "keywords", key
-        event << "키워드 '#{key}' 삭제를 완료했습니다"
-    when *keywords
-        response = redis.hget "keywords", cmd
-        event << response
+        result = redis.hdel("keywords", key)
+        if result == 1
+            event << "키워드 '#{key}' 삭제를 완료했습니다"
+        else
+            event << "그런거 없는데수우"
+        end
     else
-        event << "알 수 없는 명령어입니다"
+        response = redis.hget "keywords", cmd
+        if response
+            event << response
+        else
+            targets = keywords.select{|x| x.include? cmd}
+            case targets.length
+            when 0
+            when 1
+                keyword = targets.first
+                response = redis.hget "keywords", keyword
+                event << "!#{keyword}\n#{response}"
+            else
+                event << targets.sort.join(", ")
+            end
+        end        
     end
 end
 
